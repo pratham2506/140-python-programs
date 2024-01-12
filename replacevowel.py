92dec1911090a8aa5a4fb10ebcadfa8cce508e69
Pratham Matkar <prathammatkar2506@gmail.com>
2024-01-12 20:05:04 +0530

    All 140 codes executed successfully

---
+++ replacevowel.py
@@ -0,0 +1,10 @@
+def vow_replace(string,vowel):
+    vowels = 'aeiou'
+    result = ''
+    for char in string:
+        if char in vowels:
+            result += vowel
+        else:
+            result += char
+    return result
+print(vow_replace("stuffed jalapeno poppers", "e"))
